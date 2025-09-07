6fc5da9c5b0d169da6803592662bc8fc57ead01e
OdyPaul <aptsoriano0864@iskwela.psau.edu.ph>
2025-09-08 06:06:42 +0800

    incomplete part 4, goals CRUD 16m

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -37,14 +37,13 @@ const updateGoals =  asyncHandler(async(req, res) =>{
         throw new Error('Goal not found')
         
     }
-    //check for user
-    const user = await User.findById(req.user.id)
-    if(!user){
+
+    if(!req.user){
         res.status(401)
         throw new Error('User not found')
     }
     //Make sure the logged in user matches the goal user
-    if(goal.user.toString() !== user.id){
+    if(goal.user.toString() !== req.user.id){
         res.status(401)
         throw new Error('User not authorized')
     }
@@ -65,14 +64,12 @@ const deleteGoals =  asyncHandler(async(req, res) =>{
         throw new Error('Goal not found')
         
     }
-    //check for user
-    const user = await User.findById(req.user.id)
-    if(!user){
+    if(!req.user){
         res.status(401)
         throw new Error('User not found')
     }
     //Make sure the logged in user matches the goal user
-    if(goal.user.toString() !== user.id){
+    if(goal.user.toString() !== req.user.id){
         res.status(401)
         throw new Error('User not authorized')
     }
